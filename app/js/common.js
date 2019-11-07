730b48c026f484d462c2648e4a4bb1776a7a1863
dessid22 <dessid@yandex.ru>
2019-11-07 20:58:46 +0300

    Меню+адаптив

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,6 @@
 $( document ).ready(function(){
 
-	// Выпадающий список для выбора (по аналогии с select>option)
+	// Выпадающий список для выбора валюты (по аналогии с select>option)
 	$(".currency").on("click", ".init", function() {
     	$(this).closest(".currency").children('.currency__item:not(.init)').toggle();
 	});
@@ -16,14 +16,64 @@ $( document ).ready(function(){
 	// Закрыть выпадающий список при клике в другом месте
 	$(document).mouseup(function (e){ // событие клика по веб-документу
 		var div = $('.currency__item:not([class*="init"])'); // берем li, у которых нет класса init
-		if (!div.is(e.target) // если клик был не по нашему блоку
-		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
+		if (!div.is(e.target)) // если клик был не по нашему блоку
+ 		{
 			div.hide(); // скрываем его
 		}
 	});
 
+	// Открыть/Закрыть выпадающее меню
+	$('.basic-menu__link').on('click', function(e){
+    //внутри нашего пункта меню находим "выпадашку"
+    var $dropMenu = $(this).parent().find('.basic-menu__sub');
+    //если наш .basic-menu__sub видимый - скрываем, если скрыт - показываем
+    if($dropMenu.is(':visible')){
+        $dropMenu.hide();
+    } else {
+        //скрываем все видимые "выпадашки" на странице        
+        $('.basic-menu__sub:visible').hide(); 
+        $dropMenu.show();
+    }
+	});
+
+	// Выпадающий список для выбора карточки (по аналогии с select>option)
+	$(".my-cart").on("click", ".init", function() {
+    	$(this).closest(".my-cart").children('.my-cart__item:not(.init)').toggle();
+	});
+
+	var allOptions2 = $(".my-cart").children('.my-cart__item:not(.init)');
+	$(".my-cart").on("click", ".my-cart__item:not(.init)", function() {
+	    allOptions2.removeClass('selected');
+	    $(this).addClass('selected');
+	    $(".my-cart").children('.init').html($(this).html());
+	    allOptions2.toggle();
+	});
+
+	// Закрыть выпадающий список при клике в другом месте
+	$(document).mouseup(function (e){ // событие клика по веб-документу
+		var div2 = $('.my-cart__item:not([class*="init"])'); // берем li, у которых нет класса init
+		if (!div2.is(e.target)) // если клик был не по нашему блоку
+ 		{
+			div2.hide(); // скрываем его
+		}
+	});
+
+	// Бургер меню
+	$('.burger-link').on('click', function() {
+		$(this).toggleClass('burger-link_active');
+		$('.basic-menu').toggleClass('basic-menu_mobile');
+	});
+
+	// Закрыть мобильное меню при клике в другом месте
+	$(document).mouseup(function (e) {
+    var container = $(".basic-menu");
+    if (container.has(e.target).length === 0){ // если клик был не по .basic-menu
+        container.removeClass('basic-menu_mobile'); // скрываем за экран .basic-menu
+        $('.burger-link').removeClass('burger-link_active'); // приводим .burger-link в исходное
+    }
+	});
+
 	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
 	$("").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
